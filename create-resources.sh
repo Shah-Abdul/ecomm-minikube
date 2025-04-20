d6c330a076e722fc457876c0477df32365277d56
Shah Abdul Ghani <shah.abdul.ghani@walmart.com>
2025-04-20 23:22:20 +0530

    Refactored auth mongo DB system.

--- create-resources.sh
+++ create-resources.sh
@@ -1,7 +1,23 @@
+
+# Create namespace if it doesn't exist
+kubectl create namespace ecommerce --dry-run=client -o yaml | kubectl apply -f -
+
 # Create resources, in order:
-kubectl apply -f auth-DB-mongo-deployment.yaml -n ecommerce
-kubectl apply -f auth-DB-mongo-service.yaml -n ecommerce
+
+# Create secrets first
+kubectl apply -f ./auth-mongo-db/auth-mongo-secret.yaml -n ecommerce
+
+# Create config maps:
+kubectl apply -f ./auth-mongo-db/auth-db-mongo-init-configmap.yaml -n ecommerce
+
+# Create PVC (Persistent volume Claim):
+kubectl apply -f ./auth-mongo-db/auth-db-mongo-pvc.yaml
+
+# Create MongoDB (for Auth) deployment and service:
+kubectl apply -f ./auth-mongo-db/auth-DB-mongo-deployment.yaml -n ecommerce
+kubectl apply -f ./auth-mongo-db/auth-DB-mongo-service.yaml -n ecommerce
+
 # Access MongoDB from host machine (locally for testing):
-kubectl port-forward service/mongo 27017:27017
+kubectl port-forward service/mongo 27017:27017 -n ecommerce
 
 echo "All resources applied!"
